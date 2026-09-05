da4db9b16f410acfc4374acbdeb96379ba9fe89a
SonnerD <15979075325@163.com>
2026-09-05 22:48:20 +0800

    Add Point structure with equality operator

---
+++ include/Point.h
@@ -0,0 +1,11 @@
+#pragma once
+struct Point{
+    int row;
+    int col;
+
+    bool operator== (const Point& other) const {
+        return row == other.row &&
+         col == other.col;
+    }
+};
+ 
